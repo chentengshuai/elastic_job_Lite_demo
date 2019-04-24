518e275002b57d8cf843c3865587a27ddeb1f508
chentengshuai <729027995@qq.com>
2019-04-24 10:26:49 +0800

    4、添加业务逻辑处理

---
+++ src/main/java/com/chentengshuai/elastic_job_Lite_demo/job/StockSimpleJob.java
@@ -0,0 +1,35 @@
+package com.chentengshuai.elastic_job_Lite_demo.job;
+
+/**
+ * Copyright (C), 2019-2019,
+ * FileName: StockSimpleJob
+ * Author:   陈腾帅
+ * Date:     2019 4 24 0024 10:08
+ * Description: 添加业务逻辑处理
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 陈腾帅          修改时间           V1.0              描述
+ */
+
+import com.dangdang.ddframe.job.api.ShardingContext;
+import com.dangdang.ddframe.job.api.simple.SimpleJob;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class StockSimpleJob implements SimpleJob {
+    @Override
+    public void execute(ShardingContext shardingContext) {
+        System.out.println(String.format("------Thread ID: %s, 任务总片数: %s, " +
+                        "当前分片项: %s.当前参数: %s," +
+                        "当前任务名称: %s.当前任务参数: %s"
+                ,
+                Thread.currentThread().getId(),
+                shardingContext.getShardingTotalCount(),
+                shardingContext.getShardingItem(),
+                shardingContext.getShardingParameter(),
+                shardingContext.getJobName(),
+                shardingContext.getJobParameter()
+        ));
+    }
+}
