94de0df101e984cefeaf88f4f7f4388881720564
chentengshuai <729027995@qq.com>
2019-04-24 09:36:17 +0800

    1、引入maven坐标

---
+++ src/main/java/com/chentengshuai/elastic_job_Lite_demo/ElasticJobLiteDemoApplication.java
@@ -0,0 +1,13 @@
+package com.chentengshuai.elastic_job_Lite_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticJobLiteDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticJobLiteDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chentengshuai/elastic_job_Lite_demo/ElasticJobLiteDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chentengshuai.elastic_job_Lite_demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ElasticJobLiteDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
