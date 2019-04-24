af56b47219d9ba4a695048b8e1a93ea07ff03e3a
chentengshuai <729027995@qq.com>
2019-04-24 11:05:42 +0800

    增加日志

--- src/main/java/com/chentengshuai/elastic_job_Lite_demo/job/StockSimpleJob.java
+++ src/main/java/com/chentengshuai/elastic_job_Lite_demo/job/StockSimpleJob.java
@@ -13,14 +13,15 @@ package com.chentengshuai.elastic_job_Lite_demo.job;
 
 import com.dangdang.ddframe.job.api.ShardingContext;
 import com.dangdang.ddframe.job.api.simple.SimpleJob;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-
+@Slf4j
 @Component
 public class StockSimpleJob implements SimpleJob {
     @Override
     public void execute(ShardingContext shardingContext) {
-        System.out.println(String.format("------Thread ID: %s, 任务总片数: %s, " +
+        log.info(String.format("------Thread ID: %s, 任务总片数: %s, " +
                         "当前分片项: %s.当前参数: %s," +
                         "当前任务名称: %s.当前任务参数: %s"
                 ,
